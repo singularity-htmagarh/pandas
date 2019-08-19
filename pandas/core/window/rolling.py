5cfac0035e3d56cbeefcf04698c178eaf1b8848b
Matt Roeschke <mroeschke@housecanary.com>
2019-08-18 23:07:26 -0700

    Remove comment

--- pandas/core/window/rolling.py
+++ pandas/core/window/rolling.py
@@ -1211,7 +1211,6 @@ class _Rolling_and_Expanding(_Rolling):
     def mean(self, *args, **kwargs):
         nv.validate_window_func("mean", args, kwargs)
         return self._apply(rolling_mean, "mean", **kwargs)
-        # return self._apply("roll_mean", "mean", **kwargs)
 
     _shared_docs["median"] = dedent(
         """
